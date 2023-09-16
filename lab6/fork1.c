c7feebca49f1db616ed798493a93dc0c57b2d370
Devansh Lingamaneni <115863065+DevanshL@users.noreply.github.com>
2023-09-16 23:56:49 +0530

    Update fork1.c

--- lab6/fork1.c
+++ lab6/fork1.c
@@ -1,44 +1,84 @@
-// L.DEVANSH CS21B2023
-
-#include<stdio.h>
-#include<stdlib.h>
-#include<sys/ipc.h>
-#include<sys/shm.h>
-#include<unistd.h>
-
-#define shm_size 100
+// L Devansh CS21B2O23
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <stdlib.h>
 
 int main() {
-    int x, shmid;
-    int *shm, *s;
-    key_t key = 20232023;
-    
-    if((shmid = shmget(key, shm_size, IPC_CREAT|0666)) < 0) {       // create shared memory
-        printf("Error in shmget \n");
-        exit(1);
+    pid_t pid1, pid2, pid3, pid4;
+   
+    pid1 = fork();
+   
+    if (pid1 == -1)
+    {
+        printf("Fork failed\n");
+        exit(0);
     }
-    
-    if((shm = shmat(shmid, NULL, 0)) == (int *) -1) {       //. attach shared memory to sender
-        printf("Error in shmat \n");
-        exit(1);
+   
+    else if (pid1 == 0)
+    {                                            // Child 1 process
+        printf("Child 1 My id is %d and my parent id is %d\n", getpid(), getppid());
     }
-    
-    s = shm;            // starting pointer (shm) to 's'
-    
-    for(x = 2;x<27;x++) {
-        *s++ = x;
+    else
+    {
+       
+        wait(NULL);                                     // Wait for child 1 to complete
+       
+        pid2 = fork();
+       
+        if (pid2 == -1)
+        {
+            printf("Fork failed\n");
+            exit(0);
+        }
+       
+        else if (pid2 == 0)
+        {                                                       // Child 2 process
+            wait(NULL);
+            printf("Child 2 My id is %d and my parent id is %d\n", getpid(), getppid());
+           
+            pid3 = fork();
+           
+            if (pid3 == -1)
+            {
+                printf("Fork failed\n");
+                exit(0);
+            }
+            else if (pid3 == 0)
+            {
+                                                    // Child 3 process
+                printf("Child 3 My id is %d and my parent id is %d\n", getpid(), getppid());
+            }
+            else
+            {
+                wait(NULL);                     // Wait for child 3 to complete
+               
+                pid4 = fork();
+               
+                if (pid4 == -1)
+                {
+                    printf("Fork failed\n");
+                    exit(0);
+                }
+                else if (pid4 == 0)
+                {
+                                                     // Child 4 process
+                    printf("Child 4 My id is %d and my parent id is %d\n", getpid(), getppid());
+                }
+                else
+                {
+                    wait(NULL);                          // Wait for child 4 to complete
+                }
+            }
+        }
+        else
+        {
+            wait(NULL); // Wait for child 2 to complete
+            wait(NULL);
+            printf("Parent Process my id is %d and my parent id is %d\n", getpid(), getppid());
+        }
     }
-    
-    *s = 0;
-    
-    while(*shm != '#') {
-        sleep(1);
-        
-    }
-    
-    for(*s;*s!=0;*s++) {
-        printf(" %d ",*s);
-    }
-    
+   
     return 0;
 }

973222b23b5dba9d2ec8ac35346ea3bf6b32c005
Devansh Lingamaneni <115863065+DevanshL@users.noreply.github.com>
2023-09-22 19:24:17 +0530

    Update FCFS.c

--- lab2/FCFS.c
+++ lab2/FCFS.c
@@ -3,21 +3,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct process{
-	int pid;
-	int at;
-	int bt;
-	int ct;
-	int tat;
-	int wt;
-	int rt;
-	struct process *next;
+struct process {
+    int pid;
+    int at;
+    int bt;
+    int ct;
+    int tat;
+    int wt;
+    int rt;
+    struct process* next;
 };
 
-struct process *enqueue(struct process *newnode,struct process *rear)
-{
-	newnode->next = NULL;
-    
+struct process* enqueue(struct process* newnode, struct process* rear) {
+    newnode->next = NULL;
+
     if (rear == NULL) {
         return newnode;
     } else {
@@ -26,30 +25,25 @@ struct process *enqueue(struct process *newnode,struct process *rear)
     }
 }
 
-struct process *dequeue(struct process **front)
-{
-	if(*front == NULL)
-	{
-		return NULL;
-	}
-	else
-	{
-		struct process *temp = *front;
-		*front = (*front)->next;
-		return temp;
-	}
+struct process* dequeue(struct process** front) {
+    if (*front == NULL) {
+        return NULL;
+    } else {
+        struct process* temp = *front;
+        *front = (*front)->next;
+        return temp;
+    }
 }
 
-int main()
-{
+int main() {
     int n;
     printf("Enter the number of processes: ");
     scanf("%d", &n);
-    
+
     struct process* front = NULL;
     struct process* rear = NULL;
-    
-     for (int i = 0; i < n; i++) {
+
+    for (int i = 0; i < n; i++) {
         struct process* newnode = (struct process*)malloc(sizeof(struct process));
         printf("Enter details for Process %d:\n", i + 1);
         printf("Process ID: ");
@@ -58,52 +52,57 @@ int main()
         scanf("%d", &newnode->at);
         printf("Burst Time: ");
         scanf("%d", &newnode->bt);
-        
-        rear = enqueue( newnode,rear);   
-        if (front == NULL) 
-        {
+
+        rear = enqueue(newnode, rear);
+        if (front == NULL) {
             front = rear;
         }
-        }
-        
-        int curr_time = 0;
-        struct process *node = (struct process*)malloc(n*sizeof(struct process));
-        for(int i=0;i<n;i++)
-        {
-        	struct process *newnode = dequeue(&front);  // dequeueing first element and store in process
-        	if(newnode->at > curr_time)
-        	{
-        		curr_time = newnode->at;
-        	}
-        	
-        	newnode->ct = curr_time + newnode->bt;
-        	newnode->tat = newnode->ct - newnode->at;
-        	newnode->wt = newnode->tat - newnode->bt;
-        	newnode->rt = newnode->wt; 
-        	curr_time = newnode->ct;
-        	node[i] = *newnode;
-        	free(newnode);
-        }
-        
-        float avg_wt = 0;
-        float avg_tat = 0;
-        for(int i=0;i<n;i++)
-        {
-        	avg_wt = avg_wt + node[i].wt;
-        	avg_tat = avg_tat + node[i].tat;
-        }
-        avg_wt = avg_wt/n;
-        avg_tat = avg_tat/n;
-        printf("Pid\tAT\tBT\tCT\tTAT\tWT\tRT\n");
-        for(int i=0;i<n;i++)
-        {
-        	printf("%d\t%d\t%d\t%d\t%d\t%d\t %d\n",node[i].pid,node[i].at,node[i].bt,node[i].ct,node[i].tat,node[i].wt,node[i].rt);
-        }
-        
-         printf("Avg waiting time: %.2f\n", avg_wt);
-    	printf("Avg turnaround time: %.2f\n", avg_tat);
-    	 free(node);   
-    
-	return 0;
-}
+    }
 
+    // Corrected FCFS scheduling logic
+    int curr_time = 0;
+    struct process* node = (struct process*)malloc(n * sizeof(struct process));
+
+    // Sort the processes by arrival time
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (node[j].at > node[j + 1].at) {
+                // Swap the processes
+                struct process temp = node[j];
+                node[j] = node[j + 1];
+                node[j + 1] = temp;
+            }
+        }
+    }
+
+    for (int i = 0; i < n; i++) {
+        if (node[i].at > curr_time) {
+            curr_time = node[i].at;
+        }
+
+        node[i].ct = curr_time + node[i].bt;
+        node[i].tat = node[i].ct - node[i].at;
+        node[i].wt = node[i].tat - node[i].bt;
+        node[i].rt = node[i].wt;
+        curr_time = node[i].ct;
+    }
+
+    float avg_wt = 0;
+    float avg_tat = 0;
+    for (int i = 0; i < n; i++) {
+        avg_wt = avg_wt + node[i].wt;
+        avg_tat = avg_tat + node[i].tat;
+    }
+    avg_wt = avg_wt / n;
+    avg_tat = avg_tat / n;
+    printf("Pid\tAT\tBT\tCT\tTAT\tWT\tRT\n");
+    for (int i = 0; i < n; i++) {
+        printf("%d\t%d\t%d\t%d\t%d\t%d\t %d\n", node[i].pid, node[i].at, node[i].bt, node[i].ct, node[i].tat, node[i].wt, node[i].rt);
+    }
+
+    printf("Avg waiting time: %.2f\n", avg_wt);
+    printf("Avg turnaround time: %.2f\n", avg_tat);
+    free(node);
+
+    return 0;
+}
